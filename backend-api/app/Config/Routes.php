140d1a8d6672b07d8fcff43f82d977a9f5cccbd9
Bagus Aditya H <bagus.aditya.h.99@gmail.com>
2026-06-24 19:07:17 +0700

    Update Routes.php

--- backend-api/app/Config/Routes.php
+++ backend-api/app/Config/Routes.php
@@ -9,28 +9,28 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->options('(:any)', function() {});
 
-$routes->post('api/login', 'Api\Auth::login');
-$routes->get('api/dashboard-summary', 'Api\Dashboard::summary');
+$routes->post('api/login', 'API\Auth::login');
+$routes->get('api/dashboard-summary', 'API\Dashboard::summary');
 
 $routes->group('api', ['filter' => 'auth'], function ($routes) {
-    $routes->post('logout', 'Api\Auth::logout');
-    $routes->get('kategori', 'Api\Kategori::index');
-    $routes->post('kategori', 'Api\Kategori::create');
-    $routes->put('kategori/(:num)', 'Api\Kategori::update/$1');
-    $routes->delete('kategori/(:num)', 'Api\Kategori::delete/$1');
+    $routes->post('logout', 'API\Auth::logout');
+    $routes->get('kategori', 'API\Kategori::index');
+    $routes->post('kategori', 'API\Kategori::create');
+    $routes->put('kategori/(:num)', 'API\Kategori::update/$1');
+    $routes->delete('kategori/(:num)', 'API\Kategori::delete/$1');
 
-    $routes->get('barang', 'Api\Barang::index');
-    $routes->post('barang', 'Api\Barang::create');
-    $routes->put('barang/(:num)', 'Api\Barang::update/$1');
-    $routes->delete('barang/(:num)', 'Api\Barang::delete/$1');
+    $routes->get('barang', 'API\Barang::index');
+    $routes->post('barang', 'API\Barang::create');
+    $routes->put('barang/(:num)', 'API\Barang::update/$1');
+    $routes->delete('barang/(:num)', 'API\Barang::delete/$1');
 
-    $routes->get('supplier', 'Api\Supplier::index');
-    $routes->post('supplier', 'Api\Supplier::create');
-    $routes->put('supplier/(:num)', 'Api\Supplier::update/$1');
-    $routes->delete('supplier/(:num)', 'Api\Supplier::delete/$1');
+    $routes->get('supplier', 'API\Supplier::index');
+    $routes->post('supplier', 'API\Supplier::create');
+    $routes->put('supplier/(:num)', 'API\Supplier::update/$1');
+    $routes->delete('supplier/(:num)', 'API\Supplier::delete/$1');
 
-    $routes->get('histori', 'Api\HistoriBarang::index');
-    $routes->post('histori', 'Api\HistoriBarang::create');
-    $routes->put('histori/(:num)', 'Api\HistoriBarang::update/$1');
-    $routes->delete('histori/(:num)', 'Api\HistoriBarang::delete/$1');
+    $routes->get('histori', 'API\HistoriBarang::index');
+    $routes->post('histori', 'API\HistoriBarang::create');
+    $routes->put('histori/(:num)', 'API\HistoriBarang::update/$1');
+    $routes->delete('histori/(:num)', 'API\HistoriBarang::delete/$1');
 });
